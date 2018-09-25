52239af036130ce5531520d6b5ce5e9111f1ebfa
yangnannba <529006406@qq.com>
2018-09-25 17:57:58 +0800

    add all

---
+++ src/components/jieguo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="jieguo">
+     {{myvalue}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'jieguo',
+	props:["myvalue"],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
